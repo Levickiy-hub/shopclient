93a63dd227fa71f5b99ecc2643c3beeac41d58d8
Your Name <you@example.com>
2022-09-08 14:19:52 +0300

    navbar commit

---
+++ src/components/Search.js
@@ -0,0 +1,46 @@
+import {alpha, styled} from "@mui/material/styles";
+import {InputBase} from "@mui/material";
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 5,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(5),
+        width: '45%',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('lg')]: {
+            width: '70ch',
+            '&:focus': {
+                width: '69ch',
+            },
+        },
+    },
+}));
+
+module.exports = {Search,StyledInputBase,SearchIconWrapper}
