1d3307f04ba8c82cca1a4fe3bb7600f36a34a763
Your Name <you@example.com>
2022-09-14 23:00:41 +0300

    expansion of paths

--- src/components/appRouter.js
+++ src/components/appRouter.js
@@ -2,11 +2,19 @@ import React from 'react';
 import {Routes,Route,Navigate } from 'react-router-dom';
 import Index from '../pages/index';
 import Product from '../pages/productPage'
+import PersonalData from'../pages/userPage/personalData'
+import AboutUs from "../pages/userPage/aboutUs";
+import PointOfIssue from "../pages/userPage/pointOfIssue";
 export const AppRouter = () => {
+    // const contentName = ["personal data","delivery","discount","point of issue","payment method","return of goods",
+    //     "offer","application","contacts","about Us","sale rules"];
     return (
         <Routes>
-            <Route path={'/'} element={<Index/>} exact/>
             <Route path={'/products/:id'} element={<Product/>} exact/>
+            <Route path={'/personaldata/'} element ={<PersonalData/>} exact/>
+            <Route path={'/aboutUs/'} element ={<AboutUs/>} exact/>
+            <Route path={'/pointofissue/'} element ={<PointOfIssue/>} exact/>
+            <Route path={'/'} element={<Index/>} exact/>
             <Route path="*" element={<Navigate to ={'/'} />}/>
         </Routes>
     );
